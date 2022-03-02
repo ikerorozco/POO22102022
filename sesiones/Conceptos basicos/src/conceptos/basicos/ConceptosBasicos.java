77686daa8f53eb8031ee6aa567c60d9c019d7868
ikerorozco <ikerorozco167@aragon.unam.mx>
2022-03-02 12:36:35 -0600

    prioyceto conceptos basicos

---
+++ sesiones/Conceptos basicos/src/conceptos/basicos/ConceptosBasicos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package conceptos.basicos;
+
+/**
+ *
+ * @author ikero
+ */
+public class ConceptosBasicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola de nuevo");
+    }
+    
+}
